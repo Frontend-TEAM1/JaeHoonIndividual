2d0146f656add59502d83a2140431f8fd4eac11f
JaeHoonKOR <jazzyrain722@naver.com>
2023-01-20 19:20:53 +0900

    Add files via upload
    완성본

--- js100/asg1~10/asg10.js
+++ js100/asg1~10/asg10.js
@@ -1,5 +1,17 @@
-var y='2019';
-var month='04';
-var day='26';
-var hour='34';
-var min
+
+const num = prompt("트리의 높이를 입력해주세요 : ");
+const treeHeight=Number(num);
+
+    for(let i=1;i<=treeHeight;i++){
+        let xmasTree = "";
+        for(let j=0;j<treeHeight-i;j++){
+            xmasTree +=" ";
+
+        }
+        for(let k=0;k<2*i-1;k++){
+            xmasTree +="*";
+
+        }
+        console.log(xmasTree);
+    }
+
--- js100/asg1~10/asg8.js
+++ js100/asg1~10/asg8.js
@@ -1,3 +1,12 @@
-// 78
+var d ={
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'temperature':36,
+    'eyesight':1
+};
+
+console.log(d['weight']);
 
 
+//84
