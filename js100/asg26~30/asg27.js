4335eaa5d4929686ed54872f2243625622988214
JaeHoonKOR <jazzyrain722@naver.com>
2023-01-23 23:56:34 +0900

    Add files via upload
    27번과 30번은 아직 푸는 중입니다.. ㅠㅠ

---
+++ js100/asg26~30/asg27.js
@@ -0,0 +1,26 @@
+
+/*
+const inputStudent = prompt("공백으로 구분하여 학생의 이름을입력해주세요 : ");
+
+const studentName=inputStudent.split(" ");
+name1=studentName[0];
+name2=studentName[1];
+
+const inputScore = prompt("공백으로 구분하여 학생의 점수를 입력해주세요 : ");
+const studentScore=inputScore.split(" ");
+score1=studentScore[0];
+score2=studentScore[1];
+
+const student={
+    studentName[0]: score1,
+    name2: score2
+   
+    
+};
+
+console.log(Object.keys(student));
+*/
+
+
+
+
---
+++ js100/asg26~30/asg30.js
@@ -0,0 +1,33 @@
+
+/*
+const sentence=prompt("숨어있는 문자를 찾기위해 문자를 입력해주세요 -> ");
+
+const toFind= prompt("찾을 문자를 입력해주세요 -> ");
+
+const firstInput=sentence.split(" ");
+
+
+
+for(let i=0;i<firstInput[i].length;i++){
+    let matchScore=0;
+    for(let j=0;j<firstInput[i].length;j++){
+
+       for(let k=0;k<firstInput[i].length;k++){
+        if(toFind[j]==firstInput[i][k]){
+            matchScore+=1;
+
+            if(toFind[j+1]!=firstInput[i][k+1]){
+                matchScore-=1;
+            
+        
+        }
+
+        }
+
+       }
+
+    }
+    console.log(matchScore);
+
+}
+*/
