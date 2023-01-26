07e0fe03412f612c7d7a8c67dd599aef347fe7d4
JaeHoonKOR <jazzyrain722@naver.com>
2023-01-26 14:06:52 +0900

    기존 프로젝트를 react를 사용해 구성 중입니다.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from "react";
-import './App.css';
-import {useState} from 'react'   
-import TodoBoard from "./component/TodoBoard";
+import React from 'react';
+import './css/App.css';
+import Section1 from './component/Section1';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Section2 from './component/Section2';
+import Section3 from './component/Section3';
 
 function App() {
-  const [inputvalue,setinputvalue]=useState('');
-  const [todoList,setTodoList]=useState([]);
-  const addItem=()=>{
-    setTodoList([...todoList,inputvalue])
-  }
-  
   return (
-    <main id="main" >
-      <input
-       value={inputvalue}
-       type="text" 
-       onChange={(event)=>setinputvalue(event.target.value)
-      }/>
+    <div className="App">
+      <Header></Header>
+      <Section1></Section1>
+      <Section2></Section2>
+      <Section3></Section3>
+      <Footer></Footer>
+      
 
-      <button onClick={addItem}>추가</button>
-
-      <TodoBoard todoList={todoList}/>
-    </main>
+      
+    </div>
   );
 }
 
