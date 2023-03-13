c4b40c8095f76053931cadbec4bc0f6a1cf56ae1
JAE HOON LEE <jazzzyrain722@naver.com>
2023-03-14 08:09:33 +0900

    데이터를 불러오긴하나 아직 라이브러리에 대한 공부가 매우 많이 필요함.

--- issuesweb/src/pages/Home/components/List.js
+++ issuesweb/src/pages/Home/components/List.js
@@ -1,4 +1,23 @@
-function List() {
-  return <>List</>;
+import styled from "styled-components";
+import { flexCenter } from "../../../styles/common";
+
+function List({ issue }) {
+  return (
+    <S.Wrapper>
+      <div>{issue.id}</div>
+    </S.Wrapper>
+  );
 }
 export default List;
+
+const Wrapper = styled.div`
+  width: 80vw;
+  height: 10vh;
+
+  border: 1px solid black;
+  ${flexCenter}
+`;
+
+const S = {
+  Wrapper,
+};
--- issuesweb/src/pages/Home/index.js
+++ issuesweb/src/pages/Home/index.js
@@ -1,10 +1,34 @@
 import List from "./components/List";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 function HomePage() {
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.github.com/repos/angular/angular-cli/issues?state=all"
+        );
+        setIssues(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(issues);
+
   return (
-    <>
-      <List />
-    </>
+    <div>
+      <h1>Angular CLI Issues</h1>
+      <ul>
+        {issues.map((issue) => (
+          <li key={issue.id}>{issue.id}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
