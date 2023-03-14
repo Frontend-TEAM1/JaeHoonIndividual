92bb3331450cf5fdffc45f1153cf5157f9d81972
JAE HOON LEE <jazzzyrain722@naver.com>
2023-03-15 04:40:29 +0900

    데이터 불러오기 및 정리

--- issuesweb/src/pages/Home/components/List.js
+++ issuesweb/src/pages/Home/components/List.js
@@ -4,20 +4,58 @@ import { flexCenter } from "../../../styles/common";
 function List({ issue }) {
   return (
     <S.Wrapper>
-      <div>{issue.id}</div>
+      <S.FirstLine>
+        <div>#{issue.number}</div>
+        <div>{issue.title}</div>
+        <div>Comments{issue.comments}</div>
+      </S.FirstLine>
+      <S.Body>{issue.body}</S.Body>
+      <S.LastLine>
+        {issue.user.login}
+        {issue.created_at}
+      </S.LastLine>
     </S.Wrapper>
   );
 }
 export default List;
 
 const Wrapper = styled.div`
-  width: 80vw;
-  height: 10vh;
-
+  width: 90vw;
+  height: 20vh;
+  margin: 5vh 5vh;
+  display: flex;
+  flex-direction: column;
   border: 1px solid black;
   ${flexCenter}
 `;
 
+const FirstLine = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid green;
+`;
+const Body = styled.div`
+  border: 2px solid salmon;
+  height: 15vh;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+`;
+
+const LastLine = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  border: 2px solid deeppink;
+`;
+
 const S = {
   Wrapper,
+  FirstLine,
+  Body,
+  LastLine,
 };
--- issuesweb/src/pages/Home/index.js
+++ issuesweb/src/pages/Home/index.js
@@ -1,24 +1,35 @@
+import { Octokit } from "octokit"; // npm install octokit
 import List from "./components/List";
 import { useEffect, useState } from "react";
-import axios from "axios";
+
 function HomePage() {
   const [issues, setIssues] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.github.com/repos/angular/angular-cli/issues?state=all"
-        );
-        setIssues(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+  const getIssues = async () => {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_ACCESS_TOKEN,
+    });
 
-  console.log(issues);
+    const result = await octokit.request(
+      "GET /repos/angular/angular-cli/issues",
+      {
+        owner: "owner",
+        repo: " repo",
+        headers: {},
+        per_page: 10,
+        page: 2,
+      }
+    );
+
+    console.log(result);
+    setIssues(result.data);
+  };
+
+  console.log(issues.data);
+
+  useEffect(() => {
+    getIssues();
+  }, []);
 
   return (
     <div>
